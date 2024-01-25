6e11e7b114e676e909464054636bd5d8ddb9d173
seemrcola <seemrcola@163.com>
2024-01-25 10:30:26 +0800

    init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       user: 'ubuntu',
       host: ['101.34.87.59'], // 这里改为你的服务器 IP 数组
       ref: 'origin/main',
-      repo: 'https://github.com/seemrcola/fastify-server-template.git',
+      repo: 'git@github.com:seemrcola/fastify-server-template.git',
       path: '/var/www/recorder',
       'post-deploy': "exec bash \-l -c 'npm i && pm2 startOrReload ecosystem.config.js --env production'",
     },
